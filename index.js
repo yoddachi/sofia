5a00d67e169a5572f9f14b686b9053690780d72b
yoddachi <85811204+yoddachi@users.noreply.github.com>
2022-10-18 13:21:29 -0400

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,14 @@ yesBtn.addEventListener('click',function () {
 
 const noBtn = document.querySelector('#noBtn');
 
+noBtn.addEventListener('click',function () {
+    const randomX = parseInt(Math.random()*100);
+    const randomY = parseInt(Math.random()*100);
+    noBtn.style.setProperty('top',randomY+'%');
+    noBtn.style.setProperty('left',randomX+'%');
+    noBtn.style.setProperty('transform',`translate(-${randomX}%,-${randomY}%)`);
+});
+
 noBtn.addEventListener('mouseover', function () {
     const randomX = parseInt(Math.random()*100);
     const randomY = parseInt(Math.random()*100);
